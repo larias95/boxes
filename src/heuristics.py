ea2d7259fb7488971ae69b1ac88fc623ba3c371b
Luis Enrique <lariasec@gmail.com>
2024-10-19 00:27:44 +0200

    cache heuristic

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,16 +1,17 @@
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
-# cost_matrix = np.array([[1, 5], [2, 8]])
-
-# row_ind, col_ind = linear_sum_assignment(cost_matrix)
-
-# for pair in zip(row_ind, col_ind):
-#     print(pair)
-
 
 def min_match(dots):
+    cache = {}
+
     def h(game):
+        key = frozenset(game._boxes)
+        value = cache.get(key)
+
+        if value is not None:
+            return value
+
         D = []
         for box_x, box_y in game._boxes:
             for dot_x, dot_y in dots:
@@ -18,6 +19,8 @@ def min_match(dots):
 
         D = np.asarray(D).reshape(-1, len(dots))
         i, j = linear_sum_assignment(D)
-        return D[i, j].sum()
+        cache[key] = value = D[i, j].sum()
+
+        return value
 
     return h
