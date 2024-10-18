5ef9f6ad3cba33cedfd68bf5c4e71a85767804a0
Luis Enrique <lariasec@gmail.com>
2024-10-18 23:53:53 +0200

    added astar

--- src/search.py
+++ src/search.py
@@ -1,4 +1,5 @@
 from collections import deque
+from heapq import heappop, heappush
 
 
 class Node:
@@ -6,6 +7,17 @@ class Node:
         self.game = game
         self.move = move
         self.parent = parent
+
+
+class NodeWithCost(Node):
+    def __init__(self, game, move, parent, f, h):
+        super().__init__(game, move, parent)
+        self.f = f
+        self.h = h
+        self._cost = f + h
+
+    def __lt__(self, other):
+        return self._cost < other._cost
 
 
 def _build_solution(node):
@@ -41,5 +53,31 @@ def bfs(game):
                 return _build_solution(new_node)
 
             pending.append(new_node)
+
+    return None
+
+
+def astar(game, heur):
+    discovered = {game}
+    pending = [NodeWithCost(game, None, None, 0, 0)]
+
+    while len(pending) > 0:
+        node = heappop(pending)
+
+        for move in node.game.get_moves():
+            clone = node.game.clone()
+            clone.play(move)
+
+            if clone in discovered:
+                continue
+
+            discovered.add(clone)
+
+            new_node = NodeWithCost(clone, move, node, node.f + 1, heur(clone))
+
+            if clone.victory:
+                return _build_solution(new_node)
+
+            heappush(pending, new_node)
 
     return None
